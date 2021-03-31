483a678bbb78594696708662554113ff555f09c2
Gustavo Londoño <tavon321@gmail.com>
2021-03-31 07:57:07 -0500

    `retrieve` delivers values on non empty cache

--- Tests/FeedStoreChallengeTests.swift
+++ Tests/FeedStoreChallengeTests.swift
@@ -6,16 +6,29 @@ import XCTest
 import FeedStoreChallenge
 
 class InMemoryFeedStore: FeedStore {
+	
+	struct Cache {
+		let feed: [LocalFeedImage]
+		let timestamp: Date
+	}
+	
+	private var cache: Cache?
+	
 	func deleteCachedFeed(completion: @escaping DeletionCompletion) {
 		
 	}
 	
 	func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-		
+		self.cache = Cache(feed: feed, timestamp: timestamp)
+		completion(nil)
 	}
 	
 	func retrieve(completion: @escaping RetrievalCompletion) {
-		completion(.empty)
+		guard let cache = cache else {
+			completion(.empty)
+			return
+		}
+		completion(.found(feed: cache.feed, timestamp: cache.timestamp))
 	}
 	
 }
@@ -47,9 +60,9 @@ class FeedStoreChallengeTests: XCTestCase, FeedStoreSpecs {
 	}
 	
 	func test_retrieve_deliversFoundValuesOnNonEmptyCache() throws {
-//		let sut = try makeSUT()
-//
-//		assertThatRetrieveDeliversFoundValuesOnNonEmptyCache(on: sut)
+		let sut = try makeSUT()
+
+		assertThatRetrieveDeliversFoundValuesOnNonEmptyCache(on: sut)
 	}
 	
 	func test_retrieve_hasNoSideEffectsOnNonEmptyCache() throws {
